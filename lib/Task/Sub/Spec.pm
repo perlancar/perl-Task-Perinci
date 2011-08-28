ff521d798fae630b6c0d58c8d197cdb4955b3a43
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2011-08-28 14:20:36 +0700

    first commit, first release v0.01

---
+++ lib/Task/Sub/Spec.pm
@@ -0,0 +1,55 @@
+package Task::Sub::Spec;
+
+# VERSION
+
+# todo: Sub::Spec::Exporter
+
+1;
+# ABSTRACT: Install all Sub::Spec modules
+__END__
+
+=head1 DESCRIPTION
+
+This task will install all Sub::Spec modules.
+
+=pkgroup Included modules
+
+=pkg Sub::Spec
+
+=pkg Sub::Spec::BashComplete
+
+=pkg Sub::Spec::ByURI
+
+=pkg Sub::Spec::ByURI::http
+
+=pkg Sub::Spec::CmdLine
+
+=pkg Sub::Spec::Examples
+
+=pkg Sub::Spec::Gen::ReadTable
+
+=pkg Sub::Spec::GetArgs::Argv
+
+=pkg Sub::Spec::GetArgs::Array
+
+=pkg Sub::Spec::GetArgs::GetPost
+
+=pkg Sub::Spec::GetArgs::PathInfo
+
+=pkg Sub::Spec::HTTP
+
+=pkg Sub::Spec::HTTP::Server
+
+=pkg Sub::Spec::Runner
+
+=pkg Sub::Spec::To::HTML
+
+=pkg Sub::Spec::To::Org
+
+=pkg Sub::Spec::To::Pod
+
+=pkg Sub::Spec::To::Text
+
+=pkg Sub::Spec::Use
+
+=cut
