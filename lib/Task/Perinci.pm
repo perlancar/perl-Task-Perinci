d388297fc6c967ee856609ca7296d1c18a0028a0
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2012-01-26 16:21:29 +0700

    v0.03

---
+++ lib/Task/Perinci.pm
@@ -0,0 +1,22 @@
+package Task::Perinci;
+
+# VERSION
+
+1;
+# ABSTRACT: Install all Perinci:: modules
+
+=head1 DESCRIPTION
+
+This task will install all Perinci:: modules.
+
+=pkgroup Included modules
+
+=pkg Rinci
+
+=pkg Riap
+
+=pkg Perinci
+
+=pkg Perinci::Examples
+
+=cut
