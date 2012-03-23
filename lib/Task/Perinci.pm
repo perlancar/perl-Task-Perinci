45707f81efc93889ff3b99ef2277ee8cf298ff40
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2012-03-24 00:39:13 +0700

    Release v0.04

--- lib/Task/Perinci.pm
+++ lib/Task/Perinci.pm
@@ -11,14 +11,46 @@ This task will install all Perinci:: modules.
 
 =pkgroup Included modules
 
-=pkg Rinci
-
-=pkg Riap
-
 =pkg Perinci
+
+=pkg Perinci::BashComplete
 
 =pkg Perinci::Examples
 
 =pkg Perinci::Object
+
+=pkg Perinci::Sub::ConvertArgs::Array
+
+=pkg Perinci::Sub::DepChecker
+
+=pkg Perinci::Sub::Gen::AccessTable
+
+=pkg Perinci::Sub::GetArgs::Argv
+
+=pkg Perinci::Sub::GetArgs::Array
+
+=pkg Perinci::Sub::property::drops_privilege
+
+=pkg Perinci::Sub::property::result_postfilter
+
+=pkg Perinci::Sub::property::retry
+
+=pkg Perinci::Sub::property::timeout
+
+=pkg Perinci::Sub::Wrapper
+
+=pkg Perinci::To::HTML
+
+=pkg Perinci::To::POD
+
+=pkg Perinci::To::Text
+
+=pkg Perinci::Use
+
+=pkg Pod::Weaver::Plugin::Perinci
+
+=pkg Riap
+
+=pkg Rinci
 
 =cut
